647216505fc0d7d7d3d8cfca32bca9019e47cf25
chrram <chr.ram.94@gmail.com>
2021-10-04 12:39:19 +0200

    App completed

--- App.vue
+++ App.vue
@@ -1,17 +1,77 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
+    <text class="text-color-primary"
+      >You have ordered {{ cups }} cups of coffee</text
+    >
+    <image
+      :style="{ width: 100, height: 100 }"
+      :source="{
+        uri: 'https://64.media.tumblr.com/389eab7635fa3a13d04cb945e1974a73/tumblr_ml5xhbUWC21qg6rkio1_500.gifv',
+      }"
+    />
   </view>
 </template>
+
+<script>
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD1peEn0FDGojiMbhTl-Y-uIuSSQL_XH2s',
+  authDomain: 'mwsd-project-3b871.firebaseapp.com',
+  projectId: 'mwsd-project-3b871',
+  storageBucket: 'mwsd-project-3b871.appspot.com',
+  messagingSenderId: '888827003326',
+  appId: '1:888827003326:web:4d218192bd4ca25e2fb9ef',
+};
+
+export default {
+  data() {
+    return {
+      cups: 0
+    };
+  },
+
+  methods: {
+  },
+   async mounted() {
+    if (!firebase.default.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+
+    dbRef = firebase.default.firestore().collection('coffees');
+      dbRef
+        .doc('9IpwtC8Qh1jrlJ3STbrP')
+       try {
+      const documents = await dbRef.onSnapshot((querySnapshot) => {
+        console.log("här query snapchsot");
+
+        querySnapshot.docChanges().forEach((change) => {
+          console.log(change.doc.data().numberOrdered, "test")
+
+          this.cups = change.doc.data().numberOrdered
+        });
+      });
+    } catch (error) {
+      console.log(error, "error");
+    }
+
+    
+  }
+};
+
+</script>
 
 <style>
 .container {
   background-color: white;
   align-items: center;
-  justify-content: center;
+  margin-top: 150;
   flex: 1;
 }
 .text-color-primary {
-  color: blue;
+  color: brown;
+  padding: 45;
 }
 </style>
+
+
